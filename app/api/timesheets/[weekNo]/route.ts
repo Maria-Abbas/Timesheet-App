03fea7d23dec10430c335fc8a367ed3698b8804f
Maria Abbas <mariaaliasghar384@gmail.com>
2025-07-26 17:17:10 +0400

    api route bug fix according to nextJS15

--- app/api/timesheets/[weekNo]/route.ts
+++ app/api/timesheets/[weekNo]/route.ts
@@ -1,33 +1,13 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { weekDetails } from "@/mocks/timesheets";
-
-// export async function GET(
-//   _req: NextRequest,
-//   { params }: { params: { weekNo: string } }
-// ) {
-//   const weekNo = Number(params.weekNo);
-//   const data = weekDetails[weekNo];
-
-//   if (!data) {
-//     return NextResponse.json({ message: "Not found" }, { status: 404 });
-//   }
-
-//   return NextResponse.json(data);
-// }
-
 import { NextRequest, NextResponse } from "next/server";
 import { weekDetails } from "@/mocks/timesheets";
 
-interface RequestContext {
-  params: { weekNo: string };
-}
-
 export async function GET(
   _req: NextRequest,
-  context: RequestContext
+  { params }: { params: Promise<{ weekNo: string }> } //params is now a Promise
 ) {
-  const weekNo = Number(context.params.weekNo);
-  const data = weekDetails[weekNo];
+  const { weekNo } = await params;
+  const weekNumber = Number(weekNo);
+  const data = weekDetails[weekNumber];
 
   if (!data) {
     return NextResponse.json({ message: "Not found" }, { status: 404 });
