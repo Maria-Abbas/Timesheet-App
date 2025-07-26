9de89af1c9359e088f8eb10701647d48723481cf
Maria Abbas <mariaaliasghar384@gmail.com>
2025-07-26 16:16:28 +0400

    build success

--- app/api/timesheets/weekNo/route.ts
+++ app/api/timesheets/weekNo/route.ts
@@ -3,12 +3,14 @@ import { weekDetails } from "@/mocks/timesheets";
 
 export async function GET(
   _req: NextRequest,
-  { params }: { params: { weekNo: string } }
+  context: { params: Record<string, string> }
 ) {
-  const weekNo = Number(params.weekNo);
+  const weekNo = Number(context.params.weekNo);
   const data = weekDetails[weekNo];
+
   if (!data) {
     return NextResponse.json({ message: "Not found" }, { status: 404 });
   }
+
   return NextResponse.json(data);
 }
